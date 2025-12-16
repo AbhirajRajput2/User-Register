ce08489ecdc579590a5a39037b24be9b861f5056
Abhiraj Rajput <abhirajrajput818@gmail.com>
2025-12-16 18:46:25 +0530

    Fix Render build: move @types to dependencies

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { registerUser, loginUser,logoutUser } from "../controllers/auth.controller";
 import protect from "../middleware/auth.middleware";
+import { Request, Response } from "express";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/me", protect, (req: any, res) => {
+router.get("/me", protect, (req: Request, res: Response) => {
   res.json({
     message: "Authorized",
     phone: req.user.phone
